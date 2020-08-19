6787c5c95d1471a88cf06854dc682f8d60e5179f
KyWilkinson <wilki018@hotmail.com>
2020-08-19 14:00:39 -0500

    starting to add methods to classes

--- SodaMachine/Simulation.cs
+++ SodaMachine/Simulation.cs
@@ -14,11 +14,31 @@ namespace SodaMachine
 
 
         //Constructor
-
+        public Simulation()
+        {
+            sodaMachine = new SodaMachine();
+            customer = new Customer();
+        }
 
 
 
 
         //Memeber methods
+
+
+        //run method
+        public void RunSimulation()
+        {
+            //welcome message
+            //display wallet total
+            //display soda machine options
+            //make choice
+            //insert money for choice
+            //check inventory
+            //check to see if correct amount of money is put in
+            //dispense choice if in inventory or return money
+            //give back change if more than exact amount is put in
+            //goodbye message(enjoy your soda and please come back soon
+        }
     }
 }
