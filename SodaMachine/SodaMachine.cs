8768f92d224eb1b40859011e1be6929f59f3e2bb
KyWilkinson <wilki018@hotmail.com>
2020-08-20 11:53:52 -0500

    Finished fill register method for sodamachine class

--- SodaMachine/SodaMachine.cs
+++ SodaMachine/SodaMachine.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -31,8 +32,29 @@ namespace SodaMachine
         }
         public void FillRegister()
         {
-         
+             
+            for(int i = 0; i < 20; i++)
+            {
+                Quarter quarter = new Quarter();
+                register.Add(quarter);
+            }
+            for(int i = 0; i < 10; i++)
+            {
+                Dime dime = new Dime();
+                register.Add(dime);
+            }
+            for(int i = 0; i < 20; i++)
+            {
+                Nickle nickle = new Nickle();
+                register.Add(nickle);
+            }
+            for(int i = 0; i < 50; i++)
+            {
+                Penny penny = new Penny();
+                register.Add(penny);
+            }
         }
+
         public void FillInventory()
         {
 
